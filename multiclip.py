0f8aa65e6f9a521282f3d581fb146910e8f3ad18
nimtz <ok_goodus@yahoo.com>
2023-02-28 15:54:15 -0500

    Create multiclip.py
    basic functionality

---
+++ multiclip.py
@@ -0,0 +1,29 @@
+import pyperclip
+import keyboard
+import time
+
+secondary_clipboards = ["", "", "", "", "", "", "", "", "", "", "", ""]
+
+def copy(index):
+    global secondary_clipboards
+    keyboard.release('shift')
+    keyboard.press_and_release('ctrl+c')
+    time.sleep(0.01)
+    text = pyperclip.paste()
+    secondary_clipboards[index - 1] = text
+    print("F"+str(index),":",text)
+
+def paste(index):
+    global secondary_clipboards
+    pyperclip.copy(secondary_clipboards[index - 1])
+    keyboard.release('windows')
+    keyboard.press_and_release('v')
+        
+def register_hotkeys():
+    for i in range(1, 13):
+        keyboard.add_hotkey(f'ctrl+shift+F{i}', lambda index=i: copy(index))
+        keyboard.add_hotkey(f'ctrl+windows+F{i}', lambda index=i: paste(index))
+
+if __name__ == '__main__':
+    register_hotkeys()
+    keyboard.wait()
